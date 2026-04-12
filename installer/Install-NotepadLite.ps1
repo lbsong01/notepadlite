561558365c8fb3d897be104d49982d5345a8192f
Libin Song <lisong@microsoft.com>
2026-04-11 21:20:59 -0700

    update install/uninstall ps scripts

--- installer/Install-NotepadLite.ps1
+++ installer/Install-NotepadLite.ps1
@@ -12,11 +12,11 @@ function Test-Administrator
     return $principal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
 }
 
-function Start-ElevatedInstaller
+function Start-ElevatedScript
 {
     if (Test-Administrator)
     {
-        return
+        return $true
     }
 
     $argumentList = @(
@@ -26,8 +26,9 @@ function Start-ElevatedInstaller
         "-InstallDirectory", ('"{0}"' -f $InstallDirectory)
     )
 
+    Write-Host "Requesting administrator approval..."
     Start-Process -FilePath "powershell.exe" -Verb RunAs -ArgumentList $argumentList | Out-Null
-    exit
+    return $false
 }
 
 function Set-RegistryDefaultValue
@@ -78,6 +79,123 @@ function Set-RegistryDWordValue
     New-ItemProperty -Path $Path -Name $Name -Value $Value -PropertyType DWord -Force | Out-Null
 }
 
+function Set-ClassesStarContextMenuRegistration
+{
+    param(
+        [Parameter(Mandatory = $true)]
+        [Microsoft.Win32.RegistryHive]$Hive,
+
+        [Parameter(Mandatory = $true)]
+        [string]$MenuText,
+
+        [Parameter(Mandatory = $true)]
+        [string]$IconPath,
+
+        [Parameter(Mandatory = $true)]
+        [string]$CommandText
+    )
+
+    $baseKey = [Microsoft.Win32.RegistryKey]::OpenBaseKey($Hive, [Microsoft.Win32.RegistryView]::Registry64)
+    $menuKey = $baseKey.CreateSubKey("Software\\Classes\\*\\shell\\OpenWithNotepadLite")
+    $commandKey = $menuKey.CreateSubKey("command")
+
+    try
+    {
+        $menuKey.SetValue($null, $MenuText, [Microsoft.Win32.RegistryValueKind]::String)
+        $menuKey.SetValue("Icon", $IconPath, [Microsoft.Win32.RegistryValueKind]::String)
+        $commandKey.SetValue($null, $CommandText, [Microsoft.Win32.RegistryValueKind]::String)
+    }
+    finally
+    {
+        if ($commandKey)
+        {
+            $commandKey.Dispose()
+        }
+
+        if ($menuKey)
+        {
+            $menuKey.Dispose()
+        }
+
+        $baseKey.Dispose()
+    }
+}
+
+function Remove-ClassesStarContextMenuRegistration
+{
+    param(
+        [Parameter(Mandatory = $true)]
+        [Microsoft.Win32.RegistryHive]$Hive
+    )
+
+    $baseKey = [Microsoft.Win32.RegistryKey]::OpenBaseKey($Hive, [Microsoft.Win32.RegistryView]::Registry64)
+
+    try
+    {
+        $baseKey.DeleteSubKeyTree("Software\\Classes\\*\\shell\\OpenWithNotepadLite", $false)
+    }
+    finally
+    {
+        $baseKey.Dispose()
+    }
+}
+
+function Set-UninstallRegistration
+{
+    param(
+        [Parameter(Mandatory = $true)]
+        [string]$KeyName,
+
+        [Parameter(Mandatory = $true)]
+        [string]$DisplayName,
+
+        [Parameter(Mandatory = $true)]
+        [string]$DisplayVersion,
+
+        [Parameter(Mandatory = $true)]
+        [string]$Publisher,
+
+        [Parameter(Mandatory = $true)]
+        [string]$InstallLocation,
+
+        [Parameter(Mandatory = $true)]
+        [string]$DisplayIcon,
+
+        [Parameter(Mandatory = $true)]
+        [string]$UninstallString,
+
+        [Parameter(Mandatory = $true)]
+        [string]$QuietUninstallString
+    )
+
+    $baseKey = [Microsoft.Win32.RegistryKey]::OpenBaseKey(
+        [Microsoft.Win32.RegistryHive]::LocalMachine,
+        [Microsoft.Win32.RegistryView]::Registry64)
+    $subKey = $baseKey.CreateSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\$KeyName")
+
+    try
+    {
+        $subKey.SetValue("DisplayName", $DisplayName, [Microsoft.Win32.RegistryValueKind]::String)
+        $subKey.SetValue("DisplayVersion", $DisplayVersion, [Microsoft.Win32.RegistryValueKind]::String)
+        $subKey.SetValue("Publisher", $Publisher, [Microsoft.Win32.RegistryValueKind]::String)
+        $subKey.SetValue("InstallLocation", $InstallLocation, [Microsoft.Win32.RegistryValueKind]::String)
+        $subKey.SetValue("DisplayIcon", $DisplayIcon, [Microsoft.Win32.RegistryValueKind]::String)
+        $subKey.SetValue("UninstallString", $UninstallString, [Microsoft.Win32.RegistryValueKind]::String)
+        $subKey.SetValue("QuietUninstallString", $QuietUninstallString, [Microsoft.Win32.RegistryValueKind]::String)
+        $subKey.SetValue("NoModify", 1, [Microsoft.Win32.RegistryValueKind]::DWord)
+        $subKey.SetValue("NoRepair", 1, [Microsoft.Win32.RegistryValueKind]::DWord)
+    }
+    finally
+    {
+        if ($subKey)
+        {
+            $subKey.Dispose()
+        }
+
+        $baseKey.Dispose()
+    }
+}
+
 function New-Shortcut
 {
     param(
@@ -99,7 +217,10 @@ function New-Shortcut
     $shortcut.Save()
 }
 
-Start-ElevatedInstaller
+if (-not (Start-ElevatedScript))
+{
+    return
+}
 
 $packageRoot = $PSScriptRoot
 $payloadDirectory = Join-Path $packageRoot "App"
@@ -108,11 +229,7 @@ $installedExecutable = Join-Path $InstallDirectory "NotepadLite.App.exe"
 $startMenuShortcut = Join-Path $env:ProgramData "Microsoft\Windows\Start Menu\Programs\NotepadLite.lnk"
 $uninstallScriptSource = Join-Path $packageRoot "Uninstall-NotepadLite.ps1"
 $uninstallScriptTarget = Join-Path $InstallDirectory "Uninstall-NotepadLite.ps1"
-$contextMenuKey = "HKLM:\Software\Classes\*\shell\OpenWithNotepadLite"
-$commandKey = Join-Path $contextMenuKey "command"
 $uninstallKey = "HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\NotepadLite"
-$perUserContextMenuKey = "HKCU:\Software\Classes\*\shell\OpenWithNotepadLite"
-$perUserCommandKey = Join-Path $perUserContextMenuKey "command"
 
 if (-not (Test-Path $mainExecutable))
 {
@@ -125,8 +242,11 @@ if ($runningProcess)
     throw "Close NotepadLite before installing an update."
 }
 
+Write-Host "Installing NotepadLite to $InstallDirectory"
+
 if (Test-Path $InstallDirectory)
 {
+    Write-Host "Removing previous installation files..."
     Get-ChildItem -Path $InstallDirectory -Force | Remove-Item -Recurse -Force
 }
 else
@@ -134,29 +254,30 @@ else
     New-Item -ItemType Directory -Path $InstallDirectory -Force | Out-Null
 }
 
+Write-Host "Copying application files..."
 Copy-Item -Path (Join-Path $payloadDirectory "*") -Destination $InstallDirectory -Recurse -Force
 Copy-Item -Path $uninstallScriptSource -Destination $uninstallScriptTarget -Force
 
 $displayVersion = [System.Diagnostics.FileVersionInfo]::GetVersionInfo($installedExecutable).ProductVersion
 
-if (Test-Path $perUserContextMenuKey)
-{
-    Remove-Item -Path $perUserContextMenuKey -Recurse -Force
-}
+Remove-ClassesStarContextMenuRegistration -Hive CurrentUser
+Set-ClassesStarContextMenuRegistration `
+    -Hive LocalMachine `
+    -MenuText "Open with NotepadLite" `
+    -IconPath $installedExecutable `
+    -CommandText ('"{0}" "%1"' -f $installedExecutable)
 
-Set-RegistryDefaultValue -Path $contextMenuKey -Value "Open with NotepadLite"
-Set-RegistryStringValue -Path $contextMenuKey -Name "Icon" -Value $installedExecutable
-Set-RegistryDefaultValue -Path $commandKey -Value ('"{0}" "%1"' -f $installedExecutable)
-
-Set-RegistryStringValue -Path $uninstallKey -Name "DisplayName" -Value "NotepadLite"
-Set-RegistryStringValue -Path $uninstallKey -Name "DisplayVersion" -Value $displayVersion
-Set-RegistryStringValue -Path $uninstallKey -Name "Publisher" -Value "NotepadLite"
-Set-RegistryStringValue -Path $uninstallKey -Name "InstallLocation" -Value $InstallDirectory
-Set-RegistryStringValue -Path $uninstallKey -Name "DisplayIcon" -Value $installedExecutable
-Set-RegistryDWordValue -Path $uninstallKey -Name "NoModify" -Value 1
-Set-RegistryDWordValue -Path $uninstallKey -Name "NoRepair" -Value 1
-Set-RegistryStringValue -Path $uninstallKey -Name "UninstallString" -Value ('powershell.exe -NoProfile -ExecutionPolicy Bypass -File "{0}"' -f $uninstallScriptTarget)
-Set-RegistryStringValue -Path $uninstallKey -Name "QuietUninstallString" -Value ('powershell.exe -NoProfile -ExecutionPolicy Bypass -File "{0}"' -f $uninstallScriptTarget)
+Write-Host "Registering Windows uninstall entry..."
+$uninstallCommand = 'powershell.exe -NoProfile -ExecutionPolicy Bypass -File "{0}"' -f $uninstallScriptTarget
+Set-UninstallRegistration `
+    -KeyName "NotepadLite" `
+    -DisplayName "NotepadLite" `
+    -DisplayVersion $displayVersion `
+    -Publisher "NotepadLite" `
+    -InstallLocation $InstallDirectory `
+    -DisplayIcon $installedExecutable `
+    -UninstallString $uninstallCommand `
+    -QuietUninstallString $uninstallCommand
 
 New-Shortcut -ShortcutPath $startMenuShortcut -TargetPath $installedExecutable -Description "Launch NotepadLite"
 
--- installer/Uninstall-NotepadLite.ps1
+++ installer/Uninstall-NotepadLite.ps1
@@ -12,11 +12,11 @@ function Test-Administrator
     return $principal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
 }
 
-function Start-ElevatedInstaller
+function Start-ElevatedScript
 {
     if (Test-Administrator)
     {
-        return
+        return $true
     }
 
     $argumentList = @(
@@ -26,16 +26,37 @@ function Start-ElevatedInstaller
         "-InstallDirectory", ('"{0}"' -f $InstallDirectory)
     )
 
+    Write-Host "Requesting administrator approval..."
     Start-Process -FilePath "powershell.exe" -Verb RunAs -ArgumentList $argumentList | Out-Null
-    exit
+    return $false
 }
 
-Start-ElevatedInstaller
+function Remove-ClassesStarContextMenuRegistration
+{
+    param(
+        [Parameter(Mandatory = $true)]
+        [Microsoft.Win32.RegistryHive]$Hive
+    )
+
+    $baseKey = [Microsoft.Win32.RegistryKey]::OpenBaseKey($Hive, [Microsoft.Win32.RegistryView]::Registry64)
+
+    try
+    {
+        $baseKey.DeleteSubKeyTree("Software\\Classes\\*\\shell\\OpenWithNotepadLite", $false)
+    }
+    finally
+    {
+        $baseKey.Dispose()
+    }
+}
+
+if (-not (Start-ElevatedScript))
+{
+    return
+}
 
 $installedExecutable = Join-Path $InstallDirectory "NotepadLite.App.exe"
 $startMenuShortcut = Join-Path $env:ProgramData "Microsoft\Windows\Start Menu\Programs\NotepadLite.lnk"
-$contextMenuKey = "HKLM:\Software\Classes\*\shell\OpenWithNotepadLite"
-$perUserContextMenuKey = "HKCU:\Software\Classes\*\shell\OpenWithNotepadLite"
 $uninstallKey = "HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\NotepadLite"
 
 $runningProcess = Get-Process -Name "NotepadLite.App" -ErrorAction SilentlyContinue
@@ -44,15 +65,10 @@ if ($runningProcess)
     throw "Close NotepadLite before uninstalling it."
 }
 
-if (Test-Path $contextMenuKey)
-{
-    Remove-Item -Path $contextMenuKey -Recurse -Force
-}
+Write-Host "Removing NotepadLite integration..."
 
-if (Test-Path $perUserContextMenuKey)
-{
-    Remove-Item -Path $perUserContextMenuKey -Recurse -Force
-}
+Remove-ClassesStarContextMenuRegistration -Hive LocalMachine
+Remove-ClassesStarContextMenuRegistration -Hive CurrentUser
 
 if (Test-Path $uninstallKey)
 {
@@ -66,6 +82,7 @@ if (Test-Path $startMenuShortcut)
 
 if (Test-Path $installedExecutable)
 {
+    Write-Host "Scheduling removal of $InstallDirectory after this process exits..."
     $cleanupCommand = 'ping 127.0.0.1 -n 3 > nul & rmdir /s /q "{0}"' -f $InstallDirectory
     Start-Process -FilePath "cmd.exe" -WindowStyle Hidden -ArgumentList '/c', $cleanupCommand | Out-Null
 }
